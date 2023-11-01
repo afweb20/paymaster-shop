47d2a911061e0151f9fbfd403911406462cc7438
Anastasiia <af_web@inbox.ru>
2023-11-01 20:18:10 +0100

    new: added design

--- src/projectName.js
+++ src/projectName.js
@@ -1,1 +1,24 @@
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import "./styles/main.scss";
+
+const swiper = new Swiper(".swiper", {
+  direction: "vertical",
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
